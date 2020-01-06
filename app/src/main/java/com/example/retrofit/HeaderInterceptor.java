cbccc1a238e6569ad8ff4821c1f6be85b7d930e6
Goncharov01 <mr.sacha.goncharov.01@mail.ru>
2020-01-06 17:11:22 +0300

    Add HeaderInterceptor

---
+++ app/src/main/java/com/example/retrofit/HeaderInterceptor.java
@@ -0,0 +1,29 @@
+package com.example.retrofit;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class HeaderInterceptor implements Interceptor {
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+
+        Request original = chain.request();
+        System.out.println(original.url());
+
+        Request.Builder requestBuilder;
+        if(original.url().equals("https://petshop-server.herokuapp.com/auth")){
+            requestBuilder = original.newBuilder();
+        }else {
+            requestBuilder = original.newBuilder();
+        //            .header("Token", pm.getAuthToken());
+        }
+        System.out.println("Interceptor");
+
+        Request request = requestBuilder.build();
+        return chain.proceed(request);
+    }
+}
